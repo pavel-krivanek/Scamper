e4fdbf0fc3564ecf73cd3c4aad41ac9060ceed7b
Marius Lichtblau <marius.lichtblau@googlemail.com>
2018-04-26 16:38:39 +0200

    disable notifications

--- build-support/copyfonts.sh
+++ build-support/copyfonts.sh
@@ -1,4 +1,5 @@
-pwd
 mkdir -p /home/travis/smalltalkCI-master/_builds/Scamper/fonts
+pwd
+
 cp -R $TRAVIS_BUILD_DIR/build-support/fonts/* /home/travis/smalltalkCI-master/_builds/Scamper/fonts
 chmod -R 777 /home/travis/smalltalkCI-master/_builds/Scamper/fonts/*
